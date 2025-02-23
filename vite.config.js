43d49a01c1b719cf10247be583ae0c843008a841
PtaZeta <josemanuel.rodriguez.lorenzo@iesdonana.org>
2025-02-23 22:31:42 +0100

    inicio

--- vite.config.js
+++ vite.config.js
@@ -1,28 +1,18 @@
-import defaultTheme from 'tailwindcss/defaultTheme';
-import forms from '@tailwindcss/forms';
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
 
-/** @type {import('tailwindcss').Config} */
-export default {
-    content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
-        "./resources/**/*.blade.php",
-      "./resources/**/*.js",
-      "./resources/**/*.vue",
-      "./node_modules/flowbite/**/*.js",
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: ['resources/css/app.css', 'resources/js/app.js'],
+            refresh: true,
+        }),
     ],
-
-    theme: {
-        extend: {
-            fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
-            },
+    server: {
+        host: '0.0.0.0', // Permite acceder al servidor desde cualquier IP
+        port: 8000, // Puedes cambiar el puerto si lo deseas
+        hmr: {
+            host: '192.168.1.141', // Cambia esto a tu dominio si es necesario
         },
     },
-
-    plugins: [
-        forms,
-        require('flowbite/plugin')
-        ],
-};
+});
